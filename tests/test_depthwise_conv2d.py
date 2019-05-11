1cb3fc2c58f520e63729cb6df20d42f0d01b2c49
Syoyo Fujita <syoyo@lighttransport.com>
2019-05-11 19:33:46 +0900

    Resurrect depthwise_conv2d2x test.

--- tests/test_depthwise_conv2d.py
+++ tests/test_depthwise_conv2d.py
@@ -28,26 +28,26 @@ class TestDepthwiseConvolution2D(TFLiteModelTest):
     def test_output(self):
         self.expect(self.model, self.x)
 
-#class TestDepthwiseConvolution2DMult2(TFLiteModelTest):
-#
-#    def setUp(self):
-#
-#        class Model(chainer.Chain):
-#
-#            def __init__(self):
-#                super(Model, self).__init__()
-#                with self.init_scope():
-#
-#                    # arg = [in_chan, chan_multiplier, ksize, stride, pad]
-#                    self.l1 = L.DepthwiseConvolution2D(None, 2, 3, 1, 0)
-#
-#            def __call__(self, x):
-#                return self.l1(x)
-#
-#
-#        self.model = Model()
-#
-#        self.x = input_generator.increasing(1, 4, 5, 5)
-#
-#    def test_output(self):
-#        self.expect(self.model, self.x)
+class TestDepthwiseConvolution2DMult2(TFLiteModelTest):
+
+    def setUp(self):
+
+        class Model(chainer.Chain):
+
+            def __init__(self):
+                super(Model, self).__init__()
+                with self.init_scope():
+
+                    # arg = [in_chan, chan_multiplier, ksize, stride, pad]
+                    self.l1 = L.DepthwiseConvolution2D(None, 2, 3, 1, 0)
+
+            def __call__(self, x):
+                return self.l1(x)
+
+
+        self.model = Model()
+
+        self.x = input_generator.increasing(1, 4, 5, 5)
+
+    def test_output(self):
+        self.expect(self.model, self.x)
